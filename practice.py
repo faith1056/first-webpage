9878b3fe05b4e0939787a27e2c1860c4ab4513c1
faith1056 <faithattar10@gmail.com>
2026-09-03 16:03:26 +0100

    practice

---
+++ practice.py
@@ -0,0 +1,69 @@
+alien_color = 'green'
+if alien_color == 'green':
+    print("you have just earn 5 points")
+
+if alien_color != 'green':
+    print("failed")
+
+alien_color = 'yellow'
+if alien_color == "green":
+    print("player just earned 5 points for shooting alien")
+
+elif alien_color == 'yellow':
+    print("player just earned 10 points")
+
+
+if alien_color == 'green':
+    print("player just earned 5 points for shooting alien")
+else:
+    print("player just earned 10 points for shooting alien")
+
+
+alien_color = 'red'
+
+if alien_color == 'green':
+    print('player earned 5 points')
+
+elif alien_color == 'yellow':
+    print("player earned 10 points")
+
+
+else:
+    alien_color == 'red'
+    print("player earned 15 points")
+
+
+alien_color = 'red'
+
+if alien_color == 'green':
+    print('player earned 5 points')
+
+elif alien_color == 'yellow':
+    print("player earned 10 points")
+
+
+else:
+    alien_color == 'red'
+    print("player earned 15 points")
+
+persons = 70
+if persons < 2:
+    print("the person is a baby")
+
+elif persons < 4:
+    print("the person is a todddler")
+
+elif persons < 13:
+    print("the person is a kid")
+
+elif persons < 20:
+    print("the person is a teenager")
+
+elif persons < 65:
+    print("the person is an adult")
+
+else: 
+    print("the person is an elder")
+
+
+

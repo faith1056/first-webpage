93763bc835b637da04a19dea762daaa66b96fa09
faith1056 <faithattar10@gmail.com>
2026-09-16 10:00:32 +0100

    lambda

---
+++ lambda/exercise.py
@@ -0,0 +1,41 @@
+add_ten = lambda number: number + 10
+
+print(add_ten(5))
+
+multiply_by_three = lambda number: number * 3
+
+print(multiply_by_three(4))
+
+subtract_five =  lambda number : number - 5
+
+print(subtract_five (20))
+
+
+double_number = lambda number : number *2
+
+print(double_number(7))
+
+make_upper = lambda name : name.upper()
+
+print(make_upper("faith"))
+
+get_lenght = lambda word: len(word)
+
+print(get_lenght("python"))
+
+is_even = lambda number : number % 2 == 0
+
+print(is_even(6))
+
+is_greater_than_10 = lambda number : number > 10
+
+print(is_greater_than_10(15))
+
+
+is_small = lambda number : number < 5
+
+print(is_small(3))
+
+calculate_total = lambda price, tax : price + tax
+
+print(calculate_total(100, 20))
